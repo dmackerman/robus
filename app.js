2e2cbef7afea62e96df9fbbe21415795e6a87846
Dave Ackerman <dmackerman@gmail.com>
2013-05-24 09:44:10 -0400

    namespace properly

--- app.js
+++ app.js
@@ -1,51 +1,63 @@
-/* model */
-Workout = Backbone.Model.extend({});
+(function() {
 
-/* collection */
-var WorkoutsCollection = Backbone.Collection.extend({
-  model: Workout
-});
-
-// View for all the workouts
-var WorkoutsView = Backbone.View.extend({
-  el: $('.workout-list'),
-  tagName: 'ul',
-  initialize: function() {
-    console.log(this.collection);
-  },
-  render: function() {
-    this.collection.each(function(workout) {
-      var workoutView = new WorkoutView({ model: workout });
-      this.$el.append(workoutView.render().el); // adding all the person objects.
-    }, this);
-    return this;
+  window.App = {
+    Model: {},
+    Collection: {},
+    View: {}
   }
-});
 
-// View for a single workout
-var WorkoutView = Backbone.View.extend({
-  tagName: 'li',
-  // template: Handlebars.compile( $("workout-item").html()),
-  template: _.template($('#workout-item').html()),
-  render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;  // returning this from render method..
-  }
-});
+  /* model */
+  App.Model.Workout = Backbone.Model.extend({});
+
+  /* collection */
+  App.Collection.WorkoutsCollection = Backbone.Collection.extend({
+    model: App.Model.Workout
+  });
+
+  // View for all the workouts
+  App.View.WorkoutsView = Backbone.View.extend({
+    el: $('.workout-list'),
+    tagName: 'ul',
+    render: function() {
+      /* go through the collection and render a view for each workout item */
+      this.collection.each(function(workout) {
+        var workoutView = new App.View.WorkoutView({
+          model: workout
+        });
+        this.$el.append(workoutView.render().el); // adding all the person objects.
+      }, this);
+      return this;
+    }
+  });
+
+  // View for a single workout
+  App.View.WorkoutView = Backbone.View.extend({
+    tagName: 'li',
+    // template: Handlebars.compile( $("workout-item").html()),
+    template: _.template($('#workout-item').html()),
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this; // returning this from render method..
+    }
+  });
 
 
-var workoutsCollection = new WorkoutsCollection([{
-  name: 'Mohit Jain',
-  age: 26
-}, {
-  name: 'Taroon Tyagi',
-  age: 25,
-  occupation: 'web designer'
-}, {
-  name: 'Rahul Narang',
-  age: 26,
-  occupation: 'Java Developer'
-}]);
+  var workoutsCollection = new App.Collection.WorkoutsCollection([{
+    name: 'Back and Biceps'
+  }, {
+    name: 'Chest and Triceps'
+  }, {
+    name: 'Shoulders'
+  }, {
+    name: "Legs"
+  }, {
+    name: "Abs"
+  }]);
 
-var workoutsView = new WorkoutsView({ collection: workoutsCollection });
-$('.appView').append(workoutsView.render().el);   // adding people view in DOM.
+  var workoutsView = new App.View.WorkoutsView({
+    collection: workoutsCollection
+  });
+  $('.appView').append(workoutsView.render().el); // adding people view in DOM.
+
+})();
+
