a764283d22c4bf1d51c531398e03d5be04b6a954
Dave Ackerman <dmackerman@gmail.com>
2013-05-26 11:25:29 -0400

    starting to take shape...

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -6,9 +6,22 @@ var app = app || {};
   /* detailed view for a workout */
   app.HomeView = Jr.View.extend({
     template: Handlebars.compile($('#home-view').html()),
+    events: {
+      'click a[data-action="workouts"]': "showWorkouts"
+    },
     render: function() {
       this.$el.html(this.template());
       return this;
+    },
+    showWorkouts: function() {
+      Jr.Navigator.navigate('home', {
+        trigger: true,
+        animation: {
+          // This time slide to the right because we are going back
+          type: Jr.Navigator.animations.SLIDE_STACK,
+          direction: Jr.Navigator.directions.LEFT
+        }
+      });
     }
   });
 
--- js/views/WorkoutsView.js
+++ js/views/WorkoutsView.js
@@ -6,6 +6,9 @@ var app = app || {};
   /* the container view that contains the child view: app.WorkoutsListView */
   app.WorkoutsView = Jr.View.extend({
     template: Handlebars.compile($('#workouts-view').html()),
+    events: {
+      'click .button-prev': 'onClickButtonPrev'
+    },
     initialize: function() {
       /* initialize the container view for the list of workouts and pass it our collection */
       this.workoutsListView = new app.WorkoutsListView({ collection: new app.WorkoutsCollection() });
@@ -15,6 +18,16 @@ var app = app || {};
       /* find our .content block within the template, and render the view */
       this.$el.find('.content').html(this.workoutsListView.render().el);
       return this;
+    },
+    onClickButtonPrev: function() {
+      Jr.Navigator.navigate('home', {
+        trigger: true,
+        animation: {
+          // This time slide to the right because we are going back
+          type: Jr.Navigator.animations.SLIDE_STACK,
+          direction: Jr.Navigator.directions.RIGHT
+        }
+      });
     }
   });
 
